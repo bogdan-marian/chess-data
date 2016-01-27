426c4f867e1d1a1c448c1935d2977c24f8e501c2
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-01-27 18:42:11 +0100

    started to work on VirtualProfile

---
+++ backend/src/main/java/eu/chessdata/backend/entities/VirtualProfile.java
@@ -0,0 +1,11 @@
+package eu.chessdata.backend.entities;
+
+import com.googlecode.objectify.annotation.Entity;
+
+/**
+ * Created by Bogdan Oloeriu on 27/01/2016.
+ */
+@Entity
+public class VirtualProfile {
+
+}
