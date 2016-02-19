c9d125d4023743ee416581d293144e9b7669692d
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-02-19 23:57:41 +0100

    created add player list
    The list is populated with fake data. Time to create the necessery
    tables to put data in

---
+++ app/src/main/java/eu/chessdata/tournament/TournamentAddPlayerAdapter.java
@@ -0,0 +1,34 @@
+package eu.chessdata.tournament;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import eu.chessdata.R;
+
+/**
+ * Created by Bogdan Oloeriu on 19/02/2016.
+ */
+public class TournamentAddPlayerAdapter extends CursorAdapter{
+    public TournamentAddPlayerAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item_text,parent,false);
+        return view;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView textView = (TextView)view.findViewById(R.id.list_item_text_general_view);
+        Long longId = cursor.getLong(0);
+        textView.setText("Time to read data: "+ longId);
+    }
+
+}
--- app/src/main/java/eu/chessdata/tournament/TournamentAddPlayerFragment.java
+++ app/src/main/java/eu/chessdata/tournament/TournamentAddPlayerFragment.java
@@ -1,37 +1,53 @@
 package eu.chessdata.tournament;
 
-import android.app.Dialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
-import android.widget.ListAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import eu.chessdata.TournamentDetailsFragment;
+import eu.chessdata.R;
 
 /**
  * Created by Bogdan Oloeriu on 10/02/2016.
  */
-public class TournamentAddPlayerFragment extends DialogFragment{
+public class TournamentAddPlayerFragment extends DialogFragment implements AdapterView.OnItemClickListener{
     private String TAG = "my-debug-tag";
     private AlertDialog mAlertDialog;
+    private ListView mListView;
 
-    @NonNull
+    String[] tempItems = {"item a","item b", "item 3", "item 4","item 5","item 6","item 7","item 8","item 9"};
+
+    @Nullable
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        String stringUri = getArguments().getString(TournamentDetailsFragment.TOURNAMENT_URI);
-        Log.d(TAG, "Add player for: " + stringUri);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tournament_add_player,null,false);
+        mListView = (ListView)view.findViewById(R.id.list_view_players);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Select player to add");
+        getDialog().getWindow().setTitle("Select player");
 
-        mAlertDialog = builder.create();
-        return mAlertDialog;
+        return view;
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_text,tempItems);
+        mListView.setAdapter(adapter);
+        mListView.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        dismiss();
+        Toast.makeText(getActivity(),tempItems[position],Toast.LENGTH_SHORT).show();
     }
 }
