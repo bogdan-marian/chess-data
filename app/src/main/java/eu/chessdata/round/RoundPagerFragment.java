7b24ec2857ba22399b95b25cfa529b29637550d3
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-03-28 10:39:11 +0200

    fixed RoundPager bug

--- app/src/main/java/eu/chessdata/round/RoundPagerFragment.java
+++ app/src/main/java/eu/chessdata/round/RoundPagerFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +20,13 @@ import eu.chessdata.tools.MyGlobalTools;
  * Created by Bogdan Oloeriu on 27/03/2016.
  */
 public class RoundPagerFragment extends Fragment {
+    private static final String TAG = "my-debug-tag";
     public static final String ROUND_NUMBER = "roundPagerFragment.round.count";
 
     private ViewPager mViewPager;
     private String mStringTournamentUri;
     private String mTournamentName;
-    private int mRoundCount = 6;
+    private int mRoundCount;
 
     @Nullable
     @Override
@@ -34,11 +36,12 @@ public class RoundPagerFragment extends Fragment {
         mStringTournamentUri = getArguments().getString(TournamentDetailsFragment.TOURNAMENT_URI);
         mTournamentName = getArguments().getString(TournamentDetailsFragment.TOURNAMENT_NAME);
 
+        mRoundCount = MyGlobalTools.getTournamentTotalRounds(getContext().getContentResolver(), mStringTournamentUri);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
         mViewPager = (ViewPager) fragmentView.findViewById(R.id.container_round_pager);
         mViewPager.setAdapter(sectionsPagerAdapter);
-        mRoundCount = MyGlobalTools.getTournamentTotalRounds(getContext().getContentResolver(), mStringTournamentUri);
 
+        Log.d(TAG,"mRoundCount = " + mRoundCount);
         return fragmentView;
     }
 
