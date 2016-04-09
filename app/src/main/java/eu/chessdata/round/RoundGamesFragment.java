c6a09f9c7d65e608f8e1b6de7ca2a87d4f6ef7d0
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-04-09 14:42:33 +0200

    working on RoundGamesFragment
    ther is a bug that hinds the games

--- app/src/main/java/eu/chessdata/round/RoundGamesFragment.java
+++ app/src/main/java/eu/chessdata/round/RoundGamesFragment.java
@@ -1,34 +1,54 @@
 package eu.chessdata.round;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import eu.chessdata.R;
+import eu.chessdata.data.simplesql.GameTable;
+import eu.chessdata.data.simplesql.RoundPlayerTable;
 
 /**
  * Created by Bogdan Oloeriu on 29/03/2016.
  */
-public class RoundGamesFragment extends Fragment {
+public class RoundGamesFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final String TAG = "my-debug-tag";
+    private static final int GAMES_LOADER = 0;
 
     private String mTournamentId;
     private String mTournamentName;
 
     private String mRoundId;
     private String mRoundNumber;
+    private RoundGamesAdapter mRoundGamesAdapter;
+    private ContentResolver mContentResolver;
 
-    public static RoundGamesFragment newInstance(String tournamentId,String tournamentShortName ,String roundId, String roundNumber) {
+    public static RoundGamesFragment newInstance(String tournamentId, String tournamentShortName, String roundId, String roundNumber) {
         RoundGamesFragment roundGamesFragment = new RoundGamesFragment();
         roundGamesFragment.mTournamentId = tournamentId;
         roundGamesFragment.mTournamentName = tournamentShortName;
         roundGamesFragment.mRoundId = roundId;
         roundGamesFragment.mRoundNumber = roundNumber;
 
+
         return roundGamesFragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(GAMES_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
     }
 
     @Nullable
@@ -39,6 +59,39 @@ public class RoundGamesFragment extends Fragment {
         TextView header = (TextView) view.findViewById(R.id.round_games_header);
         header.setText("Games: (Round " + mRoundNumber + ", " + mTournamentName + ")");
 
+        mRoundGamesAdapter = new RoundGamesAdapter(getActivity(), null, 0);
+        ListView listView = (ListView) view.findViewById(R.id.round_games_list_view);
+        listView.setAdapter(mRoundGamesAdapter);
+
+        mContentResolver = getActivity().getContentResolver();
+
         return view;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri uri = GameTable.CONTENT_URI;
+        String selection = GameTable.FIELD_ROUNDID + " =?";
+        String selectionArgs[] = {mRoundId};
+        String sortOrder = GameTable.FIELD_TABLENUMBER + " ASC";
+        Loader<Cursor> cursorLoader = new CursorLoader(
+                getContext(),
+                uri,
+                null,
+                selection,
+                selectionArgs,
+                sortOrder
+        );
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mRoundGamesAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mRoundGamesAdapter.swapCursor(null);
     }
 }
