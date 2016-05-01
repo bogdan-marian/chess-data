4c8690e160f489fef39c028845b2bf06f53e5232
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-05-02 00:18:36 +0200

    implemented Tournament Servlet

---
+++ backend/src/main/java/eu/chessdata/backend/servlets/TournamentServlet.java
@@ -0,0 +1,21 @@
+package eu.chessdata.backend.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Bogdan Oloeriu on 01/05/2016.
+ */
+
+public class TournamentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher view = req.getRequestDispatcher("/html/tournament.html");
+        view.forward(req,resp);
+    }
+}
