4378d6e4632622a1d5e6a2c8444d0b5ae5af784a
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-05-08 03:00:24 +0200

    Created the landing page

--- backend/src/main/webapp/js/tournamentApp.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    var gem = {name: 'Azurite', price: 2.95};
-
-    var app = angular.module('tournamentApp',[]);
-
-    app.controller('TournamentController', function(){
-        this.product = gem;
-    });
-})();
