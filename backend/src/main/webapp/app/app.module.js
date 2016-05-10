1deca6e51712af5eb55d8ff51e818acd0db1d335
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2016-05-11 00:11:40 +0200

    created the app folder

---
+++ backend/src/main/webapp/app/app.module.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bogdan on 11/05/2016.
+ */
+
+(function () {
+    'use strict';
+    angular
+        .module('app', [
+            'ngRoute',
+            'firebase'
+        ]);
+})();
+
